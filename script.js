a73c165d06927274ba3de57f98e6a66a72022007
Robin Rhodes <bigr0b@protonmail.com>
2022-07-19 18:45:36 -0400

    Finish check for password function
    I made a function to check to see if the password and confirm password would be the same and if they don't match I used a custom validation message to tell the user they do not match

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 "use strict";
-let passValue = document.querySelector(".pass").value;
-let confirmValue = document.querySelector(".passConfirm").value;
+let passValue = document.querySelectorAll(".error")[0].value;
+let confirmValue = document.querySelectorAll(".error")[1].value;
 let button = document.querySelector("button");
+let pass = document.querySelectorAll(".error")[0];
+
+button.addEventListener("click", function (passValue, confirmValue) {
+  if (passValue !== confirmValue) {
+    pass.setCustomValidity("Passwords do not match.");
+  }
+});
